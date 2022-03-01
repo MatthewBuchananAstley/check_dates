1d89916f0b3bc2d91c3fcd5fcc0d786b30b0179e
MatthewBuchananAstley <matthewbuchanan@astley.nl>
2022-03-01 22:13:04 +0100

    added CHANGELOG file and spdx information

--- check_Dates.pl
+++ check_Dates.pl
@@ -1,4 +1,9 @@
 #!/usr/bin/perl
+# 
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# FileCopyrightText: <text> 2011 - 2022 Matthew Buchanan Astley ( matthewbuchanan@astley.nl ) </text>
+# 
 
 $in = $ARGV[0] ;
 
